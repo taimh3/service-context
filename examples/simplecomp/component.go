2d85da3433bdef91d32a9cef26bcc80bdf44b704
TaiMH3 <taimh3@fpt.com>
2024-10-15 14:01:03 +0700

    feat: init project

---
+++ examples/simplecomp/component.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+
+	sctx "github.com/taimaifika/service-context"
+)
+
+type simpleComponent struct {
+	id    string
+	value string
+}
+
+func NewSimpleComponent(id string) *simpleComponent {
+	return &simpleComponent{id: id}
+}
+
+func (s *simpleComponent) ID() string {
+	return s.id
+}
+
+func (s *simpleComponent) InitFlags() {
+	flag.StringVar(&s.value, "simple-value", "demo", "Value in string")
+}
+
+func (s *simpleComponent) Activate(_ sctx.ServiceContext) error {
+	return nil
+}
+
+func (s *simpleComponent) Stop() error {
+	return nil
+}
+
+func (s *simpleComponent) GetValue() string {
+	return s.value
+}
